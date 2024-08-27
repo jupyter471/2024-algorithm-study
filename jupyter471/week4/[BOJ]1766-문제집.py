40d571a356f2055405b41b84e6021e05d1d892cf
bokyeong <wntjdals0412@gmail.com>
2024-08-27 20:05:44 +0900

    Solve: [BOJ] 1766

--- jupyter471/week4/[BOJ]1766-문제집.py
+++ jupyter471/week4/[BOJ]1766-문제집.py
@@ -15,7 +15,7 @@ indegree = [0] * (n+1)
 pq = []
 result = []
 for _ in range(m):
-    a,b = map(int, input().split())   #a->b
+    a, b = map(int, input().split())   #a 먼저->b
     graph[a].append(b)
     indegree[b] += 1
 
@@ -23,12 +23,12 @@ for i in range(1, n+1):
     if indegree[i] == 0:
         #차수 0인 노드
         heapq.heappush(pq, i)
-    while pq:
-        nxt = heapq.heappop(pq)
-        result.append(nxt)
-        for x in graph[nxt]:
-            indegree[x] -= 1
-            if indegree[x] == 0:
-                heapq.heappush(pq, x)
+while pq:
+    nxt = heapq.heappop(pq)
+    result.append(nxt)
+    for x in graph[nxt]:
+        indegree[x] -= 1
+        if indegree[x] == 0:
+            heapq.heappush(pq, x)
 
 print(*result, sep=" ")
